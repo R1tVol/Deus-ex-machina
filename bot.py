186a65e1cac5bb1845ff896eb172451e0f5a1625
Orcun ATA <154005217+R1tVol@users.noreply.github.com>
2024-01-23 19:10:35 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import random
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,5 +20,25 @@ async def on_message(message):
         await message.channel.send("\U0001f642")
     else:
         await message.channel.send(message.content)
+
+bot = commands.Bot(command_prefix='$')
+
+
+memes = {
+    'animals': ['animal1.png', 'animal2.png'],
+    'programming': ['code1.png', 'code2.png']
+}
+
+@bot.command()
+async def meme(ctx, category=''):
+    if category.lower() in memes:
+        meme_list = memes[category.lower()]
+        meme_name = random.choice(meme_list)
+        with open(f'images/{meme_name}', 'rb') as f:
+            picture = discord.File(f)
+        await ctx.send(file=picture)
+    else:
+        await ctx.send("Belirtilen kategori bulunamadı.")
+
 
 client.run("You do not have permission to view tokens, paste your own token")
